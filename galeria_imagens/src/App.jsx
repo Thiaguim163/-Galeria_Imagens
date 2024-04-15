ae939cab7ae67892c88c2a20da7bf52994ce616a
Thiaguim163 <thiagohenriquedr16@gmail.com>
2024-04-15 10:34:29 -0300

    Api UNSPLASH

--- galeria_imagens/src/App.jsx
+++ galeria_imagens/src/App.jsx
@@ -1,15 +1,36 @@
 import FotoAmpliada from "./Components/FotoAmpliada";
 import FotoList from "./Components/Fotolist";
 import SearchBar from "./Components/SearchBar";
+
 import { useState, useEffect } from "react";
 
+import axios from 'axios;
+
 function App() {
+ const [query, setQuery] = useState('');
+ const [categoria, setCategoria] = useState('');
+
+  const fetchData = async ({ query, categoria }) => {
+    const apiKey = import.meta.enVITE_UNSPLASH_API_KEY;
+    const response = axios.get("https://api.unsplash.com/photos/random", {
+
+      params: {
+        client_id: apiKey,
+      },
+    });
+    console.log(response);
+  };
+
+  useEffect(() => {
+    fetchData(query, categoria);
+  }, []);
+
   return (
-    <>
+    <div className="container">
       <SearchBar />
       <FotoList />
       <FotoAmpliada />
-    </>
+    </div>
   );
 }
 
